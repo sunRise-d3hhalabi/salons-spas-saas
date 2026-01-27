02aefc44098c1f7cc5d02dea91caf44c2e903faa
Dana Halabi <d3hhalabi@gmail.com>
2026-01-27 03:25:59 +0300

    2 - Setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col gap-5 p-10">
+      <h1>HomePage</h1>
+      <Button className="w-max">Shadcn Button</Button>
+      <Input placeholder="Enter your name" className="w-max" />
+    </div>
+  );
 }
 
 export default HomePage;
