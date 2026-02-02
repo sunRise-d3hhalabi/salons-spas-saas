c42294021482a4df75c7fdd6dcbf9811d64f0ee5
Dana Halabi <d3hhalabi@gmail.com>
2026-02-02 12:34:44 +0300

    19 - Creating table and interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,25 @@ export interface IUser {
   created_at: string;
   updated_at: string;
 }
+
+export interface ISalon_Spa {
+  id: number;
+  name: string;
+  description: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  working_days: string[];
+  start_time: string;
+  end_time: string;
+  break_start_time: string;
+  break_end_time: string;
+  minimum_service_price: number;
+  maximum_service_price: number;
+  slot_duration: number;
+  max_bookings_per_slot: number;
+  location_name: string;
+  latitue: number;
+  longitude: number;
+}
