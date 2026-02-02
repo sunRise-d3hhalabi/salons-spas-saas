9e09d970606fc044cade44ea18b5b6f68592c571
Dana Halabi <d3hhalabi@gmail.com>
2026-02-02 13:59:57 +0300

    20 - Salon spas server actions

---
+++ src/actions/salon-spas.ts
@@ -0,0 +1,108 @@
+"use server";
+
+import supabase from "@/config/supabase-config";
+
+export const createNewSalonSpa = async (payload: any) => {
+  try {
+    const { data, error } = await supabase.from("salons-spas").insert(payload);
+    if (error) throw error;
+    return {
+      success: true,
+      message: "Salon/Spa created successfully",
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const getSalonsByOwner = async (owner_id: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("salons-spas")
+      .select("*")
+      .eq("owner_id", owner_id);
+
+    if (error) throw error;
+
+    return {
+      success: true,
+      data: data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const getSalonById = async (salon_id: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("salons-spas")
+      .select("*")
+      .eq("id", salon_id);
+
+    if (error || data.length === 0)
+      throw error || new Error("Salon/Spa not found");
+
+    return {
+      success: true,
+      data: data[0],
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const editSalonSpaById = async ({
+  id,
+  payload,
+}: {
+  payload: any;
+  id: number;
+}) => {
+  try {
+    const { data, error } = await supabase
+      .from("salons-spas")
+      .update(payload)
+      .eq("id", id);
+    if (error) throw error;
+    return {
+      success: true,
+      message: "Salon/Spa updated successfully",
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const deleteSalonSpaById = async (salon_id: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("salons-spas")
+      .delete()
+      .eq("id", salon_id);
+
+    if (error) throw error;
+
+    return {
+      success: true,
+      message: "Salon/Spa deleted successfully",
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
