51f5073db0117c90e639ce33a72ef24e861659ad
Dana Halabi <d3hhalabi@gmail.com>
2026-02-06 05:23:26 +0300

    32 - Show selected salon and spa

---
+++ src/app/(private)/user/schedule-appointment/[id]/_components/checkout.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ISalon_Spa } from "@/interfaces";
+import React from "react";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+function Checkout({ salonSpa }: { salonSpa: ISalon_Spa }) {
+  const [date, setDate] = React.useState(new Date());
+  const [time, setTime] = React.useState("09:00");
+
+  //temprary
+  const timeOptions = [
+    { label: "9:00 AM", value: "09:00" },
+    { label: "10:00 AM", value: "10:00" },
+    { label: "11:00 AM", value: "11:00" },
+    { label: "12:00 PM", value: "12:00" },
+  ];
+
+  return (
+    <div className="border border-gray-400 flex flex-col gap-5 p-5">
+      <div className="flex flex-col gap-1">
+        <span className="text-sm">Select Date</span>
+        <DatePicker
+          selected={date}
+          onChange={(value: any) => setDate(value as Date)}
+          className="border border-gray-700 p-2"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <span className="text-sm">Select Time</span>
+
+        <select
+          name=""
+          id=""
+          className="border border-gray-700 p-2"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        >
+          {timeOptions.map((time) => (
+            <option key={time.value} value={time.value}>
+              {time.label}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex justify-end">
+        <Button variant={"outline"}>Cancel</Button>
+        <Button className="ml-3">Book Appointment</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
--- src/app/(private)/user/schedule-appointment/[id]/page.tsx
+++ src/app/(private)/user/schedule-appointment/[id]/page.tsx
@@ -1,10 +1,58 @@
+import { getSalonSpaById } from "@/actions/salon-spas";
+import ErrorMessage from "@/components/ui/error-message";
 import PageTitle from "@/components/ui/page-title";
+import { ISalon_Spa } from "@/interfaces";
 import React from "react";
+import DatePicker from "react-datepicker";
 
-function BookAppointmentPage() {
+import "react-datepicker/dist/react-datepicker.css";
+import Checkout from "./_components/checkout";
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+async function BookAppointmentPage({ params }: Props) {
+  const { id }: any = await params;
+  const response = await getSalonSpaById(id);
+  if (!response.success) {
+    return <ErrorMessage error={response.message} />;
+  }
+
+  const salonSpa: ISalon_Spa = response.data;
+
+  const renderProperty = (label: string, value: any) => {
+    return (
+      <div className="flex justify-between">
+        <h1 className="text-gray-500 text-sm">{label}</h1>
+        <h1 className="text-sm font-semibold!">{value}</h1>
+      </div>
+    );
+  };
+
   return (
     <div>
       <PageTitle title="Book Appointment" />
+
+      <div className="mt-7 grid grid-cols-3 gap-10">
+        <div className="col-span-2 p-5 border border-gray-400 flex flex-col gap-1">
+          <p className="text-gray-700 text-sm">{salonSpa.description}</p>
+          <hr className="border border-gray-300 my-2" />
+
+          {renderProperty("Name", salonSpa.name)}
+          {renderProperty("Address", salonSpa.address)}
+          {renderProperty("City", salonSpa.city)}
+          {renderProperty("State", salonSpa.state)}
+          {renderProperty("Zip Code", salonSpa.zip)}
+          {renderProperty("Minimum Service Price", salonSpa.min_service_price)}
+          {renderProperty("Maximum Service Price", salonSpa.max_service_price)}
+          {renderProperty("Offer Status", salonSpa.offer_status)}
+        </div>
+
+        <div className="col-span-1">
+          <Checkout salonSpa={salonSpa} />
+        </div>
+      </div>
     </div>
   );
 }
